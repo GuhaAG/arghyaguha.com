544b4bce268f8cd3ba8e4d4e3d94d1a3714fc973
“guhaAG” <guha.agt@gmail.com>
2018-11-18 04:49:03 +0900

    added Procfile

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to new webpage.</p>
-    <p>This is a work in progress so catch you later.</p>
+    <h1>Hello world</h1>
+    <p>Welcome to my new webpage.</p>
+    <p>This is a work in progress so catch you later, hopefully very soon.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
